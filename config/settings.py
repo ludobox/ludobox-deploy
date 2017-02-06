8e88f4a13c856426e43317d7f0e5bf410ac2e1dd
Clément Renaud <clement.renaud@gmail.com>
2017-02-06 15:08:33 +0100

    update with ludobox config

--- config/settings.py
+++ config/settings.py
@@ -4,9 +4,9 @@
 import os
 from fabric.api import *
 
-MAIN_GITHUB_REP = "https://github.com/clemsos/app.junkware.io.git"
-HOME_DIR ="/home/junkware/"
-APP_NAME = "app.junkware.io"
+MAIN_GITHUB_REP = "https://github.com/ludobox/ludobox.git"
+HOME_DIR ="/home/ludobox/"
+APP_NAME = "ludobox"
 
 LOG_DIR=os.path.join(HOME_DIR,"log")
 CODE_DIR=os.path.join(HOME_DIR, APP_NAME)
@@ -17,7 +17,7 @@ VIRTUALENV_PATH=os.path.join(HOME_DIR,'venv')
 STATIC = os.path.join(CODE_DIR,'app/static')
 USE_BOWER = False
 
-WEBPORT=5010
+WEBPORT=8080
 
 RUN_DIR=os.path.join(HOME_DIR,"run")
 USGWI_SOCKET=os.path.join(RUN_DIR,".%s.sock"%APP_NAME)
@@ -25,4 +25,3 @@ TOPOGRAM_PID=os.path.join(RUN_DIR,"%s.pid"%APP_NAME)
 
 CONFIG_DIR=os.path.join(HOME_DIR,"config")
 UPLOADS_DIR=os.path.join(HOME_DIR,"uploads")
-
